3eb4613bdddfee22624d9e0d3edb2a7715d78ce6
EleonoraDM <elinor.dm@gmail.com>
2020-11-10 09:47:38 +0200

    modifying queries

--- sd-adv-querying/src/main/java/elldimi/sdadvquerying/dao/IngredientRepository.java
+++ sd-adv-querying/src/main/java/elldimi/sdadvquerying/dao/IngredientRepository.java
@@ -1,8 +1,6 @@
 package elldimi.sdadvquerying.dao;
 
 import elldimi.sdadvquerying.entities.Ingredient;
-import elldimi.sdadvquerying.entities.Shampoo;
-import org.hibernate.sql.Delete;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -18,11 +16,17 @@ public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 
     List<Ingredient> findByNameStartingWith(String beginningLetter);
 
-    List<Ingredient> findByNameIn(String ... ingredientNames);
-
-    @Transactional
-    int deleteAllByName(String wild_rose);
+    List<Ingredient> findByNameIn(String... ingredientNames);
 
     Optional<Ingredient> findByName(String ingName);
+
+    @Transactional
+    int deleteAllByName(String name);
+
+    @Transactional
+    @Modifying
+    @Query("Update Ingredient as i set i.price = i.price * :percentage where i.name IN :ingredient_names")
+    int updatePriceOfIngredientsInList(@Param("percentage") double percentage,
+                                       @Param("ingredient_names") String... ingredientNames);
 }
 
--- sd-adv-querying/src/main/java/elldimi/sdadvquerying/init/AppRunner.java
+++ sd-adv-querying/src/main/java/elldimi/sdadvquerying/init/AppRunner.java
@@ -4,7 +4,6 @@ import elldimi.sdadvquerying.dao.IngredientRepository;
 import elldimi.sdadvquerying.dao.LabelRepository;
 import elldimi.sdadvquerying.dao.ShampooRepository;
 import elldimi.sdadvquerying.entities.Ingredient;
-import elldimi.sdadvquerying.entities.Shampoo;
 import elldimi.sdadvquerying.entities.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -12,9 +11,6 @@ import org.springframework.stereotype.Component;
 import utils.PrintUtil;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
 
 @Component
 public class AppRunner implements CommandLineRunner {
@@ -29,65 +25,86 @@ public class AppRunner implements CommandLineRunner {
         this.ingredientRepository = ingredientRepository;
     }
 
-    @Transactional
     @Override
+    @Transactional
     public void run(String... args) throws Exception {
 
         //1.	Select Shampoos by Size
         this.shampooRepository
                 .findBySizeOrderById(Size.MEDIUM)
                 .forEach(PrintUtil::printShampoo);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("7--------------------------------------------------------------------------------------------------------------------------------");
+
         //7.	Select Shampoos by Ingredients
         this.shampooRepository
-                .findWithIngredientsIn("Berry")
+                .findWithIngredientsIn("Apple")
                 .forEach(PrintUtil::printShampoo);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("2--------------------------------------------------------------------------------------------------------------------------------");
+
         //2.	Select Shampoos by Size or Label
         this.shampooRepository
                 .findBySizeOrLabelIdOrderByPrice(Size.MEDIUM, (long) 10)
                 .forEach(PrintUtil::printShampoo);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("3--------------------------------------------------------------------------------------------------------------------------------");
+
         //3.	Select Shampoos by Price
         this.shampooRepository
                 .findByPriceGreaterThanOrderByPriceDesc(5.00)
                 .forEach(PrintUtil::printShampoo);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("4--------------------------------------------------------------------------------------------------------------------------------");
+
         //4.	Select Ingredients by Name
         this.ingredientRepository
                 .findByNameStartingWith("M")
                 .forEach(PrintUtil::printIngredient);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("5--------------------------------------------------------------------------------------------------------------------------------");
+
         //5.	Select Ingredients by Names
         this.ingredientRepository
                 .findByNameIn("Lavender", "Herbs", "Apple")
                 .forEach(PrintUtil::printIngredient);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("6--------------------------------------------------------------------------------------------------------------------------------");
+
         //6.	Count Shampoos by Price
         System.out.printf("Count of shampoos: %d%n",
                 (long) this.shampooRepository
                         .findByPriceLessThan(8.50)
                         .size());
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("8--------------------------------------------------------------------------------------------------------------------------------");
+
         //8.	Select Shampoos by Ingredients Count
         this.shampooRepository
                 .findAllByIngredientsCount(2)
                 .forEach(PrintUtil::printShampoo);
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------------");
+        System.out.println("9-------------------------------------------------------------------------------------------------------------------------------");
+
         //9.	Delete Ingredients by Name
-        String ingName = "Berry";
+        String ingName = "Apple";
 
-        Ingredient ingToDelete = this.ingredientRepository.findByName(ingName).get();
-
-        int macadamia_oil =
+        int shampoosCount =
                 this.shampooRepository
                         .findWithIngredientsIn(ingName).size();
 
+        Ingredient ingToDelete = this.ingredientRepository.findByName(ingName).get();
         this.shampooRepository
                 .findWithIngredientsIn(ingName)
                 .forEach(s -> s.getIngredients().remove(ingToDelete));
 
-        System.out.printf("Deleted %d ingredient/-s with the given name!", macadamia_oil);
+        int deletedIngs = this.ingredientRepository.deleteAllByName(ingName);
 
+        System.out.printf("Deleted %d ingredient/-s with the given name! Removed from %d shampoos!%n",
+                deletedIngs, shampoosCount);
+        System.out.println("11 before------------------------------------------------------------------------------------------------------------------------");
+
+        //11.	Update Ingredients by Names
+        this.ingredientRepository
+                .findByNameIn("Lavender", "Herbs", "Apple")
+                .forEach(PrintUtil::printIngredient);
+        this.ingredientRepository
+                .updatePriceOfIngredientsInList(1.80, "Lavender", "Herbs", "Apple");
+        System.out.println("11 after-------------------------------------------------------------------------------------------------------------------------");
+        this.ingredientRepository
+                .findByNameIn("Lavender", "Herbs")
+                .forEach(PrintUtil::printIngredient);
     }
 }
